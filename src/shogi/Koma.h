19b421cd877f56700ed003c903bc8dfd3a964e88
kdr250 <54400975+kdr250@users.noreply.github.com>
2026-09-24 13:11:06 +0900

    add koma class

---
+++ src/shogi/Koma.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <string>
+
+/**
+ * 駒の種類
+ */
+enum class KomaType {
+    Fu, // 歩
+    Tokin, // と金
+    Kyo, // 香
+    NariKyo, // 成香
+    Keima, // 桂
+    NariKei, // 成桂
+    Gin, // 銀
+    NariGin, // 成銀
+    Kin, // 金
+    Hisha, // 飛車
+    Ryu, // 龍
+    Kaku, // 角
+    Uma, // 馬
+    Gyoku, // 玉
+};
+
+/**
+ * 駒
+ */
+class Koma {
+private:
+    KomaType mType;
+
+public:
+    inline Koma(KomaType type)
+        : mType(type) { };
+
+    /**
+     * 文字にする
+     */
+    inline std::string ToString()
+    {
+        switch (mType) {
+            case KomaType::Fu:
+                return "歩";
+            case KomaType::Tokin:
+                return "と";
+
+            case KomaType::Kyo:
+                return "香";
+            case KomaType::NariKyo:
+                return "杏";
+
+            case KomaType::Keima:
+                return "桂";
+            case KomaType::NariKei:
+                return "圭";
+
+            case KomaType::Gin:
+                return "銀";
+            case KomaType::NariGin:
+                return "全";
+
+            case KomaType::Kin:
+                return "金";
+
+            case KomaType::Hisha:
+                return "飛";
+            case KomaType::Ryu:
+                return "竜";
+
+            case KomaType::Kaku:
+                return "角";
+            case KomaType::Uma:
+                return "馬";
+
+            case KomaType::Gyoku:
+                return "玉";
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 成る
+     */
+    inline KomaType Naru()
+    {
+        switch (mType) {
+            case KomaType::Fu:
+                return KomaType::Tokin;
+
+            case KomaType::Kyo:
+                return KomaType::NariKyo;
+
+            case KomaType::Keima:
+                return KomaType::NariKei;
+
+            case KomaType::Gin:
+                return KomaType::NariGin;
+
+            case KomaType::Hisha:
+                return KomaType::Ryu;
+
+            case KomaType::Kaku:
+                return KomaType::Uma;
+
+            default:
+                break;
+        }
+    }
+};
